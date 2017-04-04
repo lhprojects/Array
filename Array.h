0e0de7e227d2051cd19ea1690730d8a3374b1a0c
lihawl <lihawl@126.com>
2017-04-04 18:15:17 +0800

    bug fix etc

--- Array.h
+++ Array.h
@@ -7,28 +7,51 @@
 #	endif
 #endif
 
+#include <type_traits>
+
+#define ARRAY_NOEXCEPT 
+
 namespace lah {
 
 	template<class TYPE>
 	struct Array1 {
 
-		Array1() noexcept;
+		Array1() ARRAY_NOEXCEPT;
 		Array1(int N1) : Array1() { alloc(N1); }
+		Array1(Array1 const &r);
+		Array1 &operator=(Array1 const &r);
+		Array1(Array1 &&r) : Array1() ARRAY_NOEXCEPT { swap(r); }
+		Array1 &operator=(Array1 &&r) ARRAY_NOEXCEPT { swap(r); }
+		~Array1();
+
+		void swap(Array1 &r) ARRAY_NOEXCEPT { 
+			std::swap(fN1, r.fN1);
+			auto t = r.fElements;
+			r.fElements = fElements;
+			fElements = t;
+		}
 		void alloc(int N1);
-		void dealloc() noexcept;
+		void dealloc() ARRAY_NOEXCEPT;
+
 		TYPE &at(int n1);
 		TYPE &operator()(int n1);
 		TYPE const &operator()(int n1) const;
-		void zeroize();
-		void assign_zero();
-		TYPE *begin() { return fElements; }
-		void copy(TYPE const *data, int N);
-		void copy(Array1<TYPE> const &r);
+
 		int size1() const;
-		~Array1();
+		TYPE *data() { return fElements; }
+		TYPE const *data() const { return fElements; }
+
+		TYPE *begin() { return fElements; }
+		TYPE const *begin() const { return fElements; }
+		TYPE const *cbegin() const { return fElements; }
+		TYPE *end() { return fElements + fN1; }
+		TYPE const *end() const { return fElements + fN1; }
+		TYPE const *cend() const { return fElements + fN1; }
+		int size() { return fN1; }
+
+		void copy(TYPE const *data, int n);
 	private:
-		Array1 &operator=(Array1 const &r);
-		Array1(Array1 const &r);
+		void deconstruct() ARRAY_NOEXCEPT;
 		int fN1;
 		TYPE *fElements;
 	};
@@ -36,20 +59,22 @@ namespace lah {
 	template<class TYPE>
 	struct Array2 {
 
-		Array2() noexcept;
+		Array2() ARRAY_NOEXCEPT;
 		Array2(int N1, int N2) : Array2() { alloc(N1, N2); }
+		Array2 &operator=(Array2 const &r);
+		Array2(Array2 const &r);
 		void alloc(int N1, int N2);
-		void dealloc();
+		void dealloc() ARRAY_NOEXCEPT;
 		TYPE &at(int n1, int n2);
 		TYPE &operator()(int n1, int n2);
 		TYPE const &operator()(int n1, int n2) const;
 		void zeroize();
 		void assign_zero();
 		int size1() const;
+		int size2() const { return fN2; }
 		~Array2();
 	private:
-		Array2 &operator=(Array2 const &r);
-		Array2(Array2 const &r);
+		void deconstruct() ARRAY_NOEXCEPT;
 		int fN1; int fN2;
 		TYPE *fElements;
 	};
@@ -57,20 +82,29 @@ namespace lah {
 	template<class TYPE>
 	struct Array3 {
 
-		Array3() noexcept;
+		Array3() ARRAY_NOEXCEPT;
 		Array3(int N1, int N2, int N3) : Array3() { alloc(N1, N2, N3); }
+		Array3 &operator=(Array3 const &r);
+		Array3(Array3 const &r);
+		
 		void alloc(int N1, int N2, int N3);
-		void dealloc();
+		void dealloc() ARRAY_NOEXCEPT;
+		void zeroize() {
+			for (int i = 0; i < fN1*fN2*fN3; ++i) {
+				fElements[i] = 0;
+			}
+		}
+
 		TYPE &at(int n1, int n2, int n3);
 		TYPE &operator()(int n1, int n2, int n3);
 		TYPE const &operator()(int n1, int n2, int n3) const;
-		void zeroize();
-		void assign_zero();
 		int size1() const;
+		int size2() const { return fN2; }
+		int size3() const { return fN3; }
+
 		~Array3();
 	private:
-		Array3 &operator=(Array3 const &r);
-		Array3(Array3 const &r);
+		void deconstruct() ARRAY_NOEXCEPT;
 		int fN1; int fN2; int fN3;
 		TYPE *fElements;
 	};
@@ -78,10 +112,12 @@ namespace lah {
 	template<class TYPE>
 	struct Array4 {
 
-		Array4() noexcept;
+		Array4() ARRAY_NOEXCEPT;
 		Array4(int N1, int N2, int N3, int N4) : Array4() { alloc(N1, N2, N3, N4); }
+		Array4 &operator=(Array4 const &r);
+		Array4(Array4 const &r);
 		void alloc(int N1, int N2, int N3, int N4);
-		void dealloc();
+		void dealloc() ARRAY_NOEXCEPT;
 		TYPE &at(int n1, int n2, int n3, int n4);
 		TYPE &operator()(int n1, int n2, int n3, int n4);
 		TYPE const &operator()(int n1, int n2, int n3, int n4) const;
@@ -92,8 +128,7 @@ namespace lah {
 		int size1() const;
 		~Array4();
 	private:
-		Array4 &operator=(Array4 const &r);
-		Array4(Array4 const &r);
+		void deconstruct() ARRAY_NOEXCEPT;
 		int fN1; int fN2; int fN3; int fN4;
 		TYPE *fElements;
 	};
@@ -101,18 +136,20 @@ namespace lah {
 	template<class TYPE>
 	struct Array5 {
 
-		Array5() noexcept;
+		Array5() ARRAY_NOEXCEPT;
 		Array5(int N1, int N2, int N3, int N4,int N5) : Array5() { alloc(N1, N2, N3, N4, N5); }
+		Array5 &operator=(Array5 const &r);
+		Array5(Array5 const &r);
 		void alloc(int N1, int N2, int N3, int N4,int N5);
-		void dealloc();
+		void dealloc() ARRAY_NOEXCEPT;
 		TYPE &at(int n1, int n2, int n3, int n4, int n5);
-		TYPE const &operator()(int n1, int n2, int n3, int n4, int n5) const;
+		TYPE &operator()(int n1, int n2, int n3, int n4, int n5) ARRAY_NOEXCEPT;
+		TYPE const &operator()(int n1, int n2, int n3, int n4, int n5) const ARRAY_NOEXCEPT;
 		void zeroize();
 		void assign_zero();
 		~Array5();
+		void deconstruct() ARRAY_NOEXCEPT;
 	private:
-		Array5 &operator=(Array5 const &r);
-		Array5(Array5 const &r);
 		int fN1; int fN2; int fN3; int fN4; int fN5;
 		TYPE *fElements;
 	};
@@ -120,18 +157,19 @@ namespace lah {
 	template<class TYPE>
 	struct Array6 {
 
-		Array6() noexcept;
+		Array6() ARRAY_NOEXCEPT;
 		Array6(int N1, int N2, int N3, int N4, int N5, int N6) : Array6() { alloc(N1, N2, N3, N4, N5, N6); }
+		Array6 &operator=(Array6 const &r);
+		Array6(Array6 const &r);
 		void alloc(int N1, int N2, int N3, int N4, int N5, int N6);
-		void dealloc();
+		void dealloc() ARRAY_NOEXCEPT;
 		TYPE &at(int n1, int n2, int n3, int n4, int n5, int n6);
-		TYPE const &operator()(int n1, int n2, int n3, int n4, int n5, int n6) const;
+		TYPE &operator()(int n1, int n2, int n3, int n4, int n5, int n6) ARRAY_NOEXCEPT;
+		TYPE const &operator()(int n1, int n2, int n3, int n4, int n5, int n6) const ARRAY_NOEXCEPT;
 		void zeroize();
 		void assign_zero();
 		~Array6();
 	private:
-		Array6 &operator=(Array6 const &r);
-		Array6(Array6 const &r);
 		int fN1; int fN2; int fN3; int fN4; int fN5; int fN6;
 		TYPE *fElements;
 	};
@@ -139,19 +177,20 @@ namespace lah {
 	template<class TYPE>
 	struct Array7 {
 
-		Array7() noexcept;
+		Array7() ARRAY_NOEXCEPT;
 		Array7(int N1, int N2, int N3, int N4, int N5, int N6, int N7) : Array7() { alloc(N1, N2, N3, N4, N5, N6, N7); }
+		Array7 &operator=(Array7 const &r);
+		Array7(Array7 const &r);
 		void alloc(int N1, int N2, int N3, int N4, int N5, int N6, int N7);
-		void dealloc();
+		void dealloc() ARRAY_NOEXCEPT;
 		TYPE &at(int n1, int n2, int n3, int n4, int n5, int n6, int n7);
-		TYPE const &operator()(int n1, int n2, int n3, int n4, int n5, int n6, int n7) const;
+		TYPE &operator()(int n1, int n2, int n3, int n4, int n5, int n6, int n7) ARRAY_NOEXCEPT;
+		TYPE const &operator()(int n1, int n2, int n3, int n4, int n5, int n6, int n7) const ARRAY_NOEXCEPT;
 		void zeroize();
 		void assign_zero();
 		int size1() const;
 		~Array7();
 	private:
-		Array7 &operator=(Array7 const &r);
-		Array7(Array7 const &r);
 		int fN1; int fN2; int fN3; int fN4; int fN5; int fN6; int fN7;
 		TYPE *fElements;
 	};
--- Array.inc.h
+++ Array.inc.h
@@ -5,106 +5,108 @@
 #endif
 #endif
 
+#ifndef ARRAY_ERROR
+#	define ARRAY_ERROR(x) if(!(x)) { printf("%s", #x);}
+#endif
 namespace lah {
 
 	template<class TYPE>
-	Array1<TYPE>::Array1() {
+	Array1<TYPE>::Array1() ARRAY_NOEXCEPT {
 		fElements = nullptr;
 		fN1 = 0;
 	}
 	template<class TYPE>
-	Array2<TYPE>::Array2() {
+	Array2<TYPE>::Array2() ARRAY_NOEXCEPT {
 		fElements = nullptr;
 		fN1 = 0; fN2 = 0;
 	}
 	template<class TYPE>
-	Array3<TYPE>::Array3() {
+	Array3<TYPE>::Array3() ARRAY_NOEXCEPT {
 		fElements = nullptr;
 		fN1 = 0; fN2 = 0; fN3 = 0;
 	}
 	template<class TYPE>
-	Array4<TYPE>::Array4() {
+	Array4<TYPE>::Array4() ARRAY_NOEXCEPT {
 		fElements = nullptr;
 		fN1 = 0; fN2 = 0; fN3 = 0; fN4 = 0;
 	}
 	template<class TYPE>
-	Array5<TYPE>::Array5() {
+	Array5<TYPE>::Array5() ARRAY_NOEXCEPT {
 		fElements = nullptr;
 		fN1 = 0; fN2 = 0; fN3 = 0; fN4 = 0; fN5=0;
 	}
 	template<class TYPE>
-	Array6<TYPE>::Array6() {
+	Array6<TYPE>::Array6() ARRAY_NOEXCEPT {
 		fElements = nullptr;
 		fN1 = 0; fN2 = 0; fN3 = 0; fN4 = 0; fN5=0; fN6=0;
 	}
 	template<class TYPE>
-	Array7<TYPE>::Array7() {
+	Array7<TYPE>::Array7() ARRAY_NOEXCEPT {
 		fElements = nullptr;
 		fN1 = 0; fN2 = 0; fN3 = 0; fN4 = 0; fN5=0; fN6=0; fN7=0;
 	}
 
 	template<class TYPE>
+	void Array1<TYPE>::copy(TYPE const * data, int n) {
+		alloc(n);
+		for (int i = 0; i < n; ++i) {
+			at(i) = data[i];
+		}
+	}
+
+	template<class TYPE>
 	void Array1<TYPE>::alloc(int N1) {
-		if(fElements == nullptr || fN1 != N1) {
+		if (fElements == nullptr || fN1 != N1) {
 			dealloc();
-			fElements = new TYPE[N1];
+			// http://en.cppreference.com/w/cpp/language/value_initialization
+			// 3) if T is an array type, each element of the array is value-initialized
+			// 4) otherwise, the object is zero-initialized
+			fElements = new TYPE[N1]();
+			fN1 = N1;
 		} else {
 			for (int i = 0; i < fN1; ++i) {
 				fElements[i].~TYPE();
 			}
-			new(fElements) TYPE[fN1];
-		}
-		fN1 = N1;
-		if (std::is_trivially_constructible<TYPE>::value) {
-			zeroize();
+			new(fElements) TYPE[fN1]();
 		}
 	}
 	template<class TYPE>
 	void Array2<TYPE>::alloc(int N1, int N2) {
 		if(fElements == nullptr||fN1 != N1||fN2 != N2) {
 			dealloc();
-			fElements = new TYPE[N1*N2];
+			fElements = new TYPE[N1*N2]();
+			fN1 = N1; fN2 = N2;
 		} else {
 			for (int i = 0; i < fN1*fN2; ++i) {
 				fElements[i].~TYPE();
 			}
-			new(fElements) TYPE[fN1*fN2];
-		}
-		fN1 = N1; fN2 = N2;
-		if (std::is_trivially_constructible<TYPE>::value) {
-			zeroize();
+			new(fElements) TYPE[fN1*fN2]();
 		}
 	}
 	template<class TYPE>
 	void Array3<TYPE>::alloc(int N1, int N2, int N3) {
 		if(fElements == nullptr||fN1 != N1||fN2 != N2||fN3 != N3) {
 			dealloc();
-			fElements = new TYPE[N1*N2*N3];
+			fElements = new TYPE[N1*N2*N3]();
+			fN1 = N1; fN2 = N2; fN3 = N3;
 		} else {
 			for (int i = 0; i < fN1*fN2*fN3; ++i) {
 				fElements[i].~TYPE();
 			}
-			new(fElements) TYPE[fN1*fN2*fN3];
-		}
-		fN1 = N1; fN2 = N2; fN3 = N3;
-		if (std::is_trivially_constructible<TYPE>::value) {
-			zeroize();
+			new(fElements) TYPE[fN1*fN2*fN3]();
 		}
 	}
 	template<class TYPE>
 	void Array4<TYPE>::alloc(int N1, int N2, int N3, int N4) {
 		if(fElements == nullptr||fN1 != N1||fN2 != N2||fN3 != N3||fN4 != N4) {
 			dealloc();
-			fElements = new TYPE[N1*N2*N3*N4];
+			fElements = new TYPE[N1*N2*N3*N4]();
+			fN1 = N1; fN2 = N2; fN3 = N3; fN4 = N4;
 		} else {
 			for (int i = 0; i < fN1*fN2*fN3*fN4; ++i) {
 				fElements[i].~TYPE();
 			}
-			new(fElements) TYPE[fN1*fN2*fN3*fN4];
-		}
-		fN1 = N1; fN2 = N2; fN3 = N3; fN4 = N4;
-		if (std::is_trivially_constructible<TYPE>::value) {
-			zeroize();
+			new(fElements) TYPE[fN1*fN2*fN3*fN4]();
 		}
 	}
 	template<class TYPE>
@@ -112,16 +114,13 @@ namespace lah {
 		if(fElements == nullptr||fN1 != N1||fN2 != N2||fN3 != N3||fN4 != N4
 			|| fN5 != N5) {
 			dealloc();
-			fElements = new TYPE[N1*N2*N3*N4*N5];
+			fElements = new TYPE[N1*N2*N3*N4*N5]();
+			fN1 = N1; fN2 = N2; fN3 = N3; fN4 = N4; fN5 = N5;
 		} else {
 			for (int i = 0; i < fN1*fN2*fN3*fN4*fN5; ++i) {
 				fElements[i].~TYPE();
 			}
-			new(fElements) TYPE[fN1*fN2*fN3*fN4*fN5];
-		}
-		fN1 = N1; fN2 = N2; fN3 = N3; fN4 = N4; fN5 = N5;
-		if (std::is_trivially_constructible<TYPE>::value) {
-			zeroize();
+			new(fElements) TYPE[fN1*fN2*fN3*fN4*fN5]();
 		}
 	}
 	template<class TYPE>
@@ -129,16 +128,13 @@ namespace lah {
 		if(fElements == nullptr||fN1 != N1||fN2 != N2||fN3 != N3||fN4 != N4
 			||fN5 != N5||fN6 != N6) {
 			dealloc();
-			fElements = new TYPE[N1*N2*N3*N4*N5*N6];
+			fElements = new TYPE[N1*N2*N3*N4*N5*N6]();
+			fN1 = N1; fN2 = N2; fN3 = N3; fN4 = N4; fN5 = N5; fN6 = N6;
 		} else {
 			for (int i = 0; i < fN1*fN2*fN3*fN4*fN5*fN6; ++i) {
 				fElements[i].~TYPE();
 			}
-			new(fElements) TYPE[fN1*fN2*fN3*fN4*fN5*fN6];
-		}
-		fN1 = N1; fN2 = N2; fN3 = N3; fN4 = N4; fN5 = N5; fN6 = N6;
-		if (std::is_trivially_constructible<TYPE>::value) {
-			zeroize();
+			new(fElements) TYPE[fN1*fN2*fN3*fN4*fN5*fN6]();
 		}
 	}
 	template<class TYPE>
@@ -146,21 +142,55 @@ namespace lah {
 		if(fElements == nullptr||fN1 != N1||fN2 != N2||fN3 != N3||fN4 != N4
 			||fN5 != N5||fN6 != N6||fN7 != N7) {
 			dealloc();
-			fElements = new TYPE[N1*N2*N3*N4*N5*N6*N7];
+			fElements = new TYPE[N1*N2*N3*N4*N5*N6*N7]();
+			fN1 = N1; fN2 = N2; fN3 = N3; fN4 = N4; fN5 = N5; fN6 = N6; fN7 = N7;
 		} else {
 			for (int i = 0; i < fN1*fN2*fN3*fN4*fN5*fN6*fN7; ++i) {
 				fElements[i].~TYPE();
 			}
-			new(fElements) TYPE[fN1*fN2*fN3*fN4*fN5*fN6*fN7];
-		}
-		fN1 = N1; fN2 = N2; fN3 = N3; fN4 = N4; fN5 = N5; fN6 = N6; fN7 = N7;
-		if (std::is_trivially_constructible<TYPE>::value) {
-			zeroize();
+			new(fElements) TYPE[fN1*fN2*fN3*fN4*fN5*fN6*fN7]();
 		}
 	}
 
 	template<class TYPE>
-	void Array1<TYPE>::dealloc() {
+	Array1<TYPE> &Array1<TYPE>::operator=(Array1<TYPE> const &r) {
+		if (fElements == nullptr || fN1 != r.fN1) {
+			dealloc();
+			fElements = new TYPE[fN1]();
+			fN1 = r.fN1;
+		}
+		for (int i = 0; i < fN1; ++i) {
+			fElements[i] = r.fElements[i];
+		}
+		return *this;
+	}
+	template<class TYPE>
+	Array2<TYPE> &Array2<TYPE>::operator=(Array2<TYPE> const &r) {
+		if (fElements == nullptr || fN1 != r.fN1 || fN2 != r.fN2) {
+			dealloc();
+			fElements = new TYPE[fN1*fN2]();
+			fN1 = r.fN1; fN2 = r.fN2;
+		}
+		for (int i = 0; i < fN1*fN2; ++i) {
+			fElements[i] = r.fElements[i];
+		}
+		return *this;
+	}
+	template<class TYPE>
+	Array3<TYPE> &Array3<TYPE>::operator=(Array3<TYPE> const &r) {
+		if (fElements == nullptr || fN1 != r.fN1 || fN2 != r.fN2 || fN3 != r.fN3) {
+			dealloc();
+			fElements = new TYPE[fN1*fN2*fN3]();
+			fN1 = r.fN1; fN2 = r.fN2; fN3 = r.fN3;
+		}
+		for (int i = 0; i < fN1*fN2*fN3; ++i) {
+			fElements[i] = r.fElements[i];
+		}
+		return *this;
+	}
+
+	template<class TYPE>
+	void Array1<TYPE>::dealloc() ARRAY_NOEXCEPT {
 		if(fElements != nullptr) {
 			delete[] fElements;
 			fElements = nullptr;
@@ -168,7 +198,7 @@ namespace lah {
 		}
 	}
 	template<class TYPE>
-	void Array2<TYPE>::dealloc() {
+	void Array2<TYPE>::dealloc() ARRAY_NOEXCEPT {
 		if(fElements != nullptr) {
 			delete[] fElements;
 			fElements = nullptr;
@@ -176,7 +206,7 @@ namespace lah {
 		}
 	}
 	template<class TYPE>
-	void Array3<TYPE>::dealloc() {
+	void Array3<TYPE>::dealloc() ARRAY_NOEXCEPT {
 		if(fElements != nullptr) {
 			delete[] fElements;
 			fElements = nullptr;
@@ -184,7 +214,7 @@ namespace lah {
 		}
 	}
 	template<class TYPE>
-	void Array4<TYPE>::dealloc() {
+	void Array4<TYPE>::dealloc() ARRAY_NOEXCEPT {
 		if(fElements != nullptr) {
 			delete[] fElements;
 			fElements = nullptr;
@@ -192,7 +222,7 @@ namespace lah {
 		}
 	}
 	template<class TYPE>
-	void Array5<TYPE>::dealloc() {
+	void Array5<TYPE>::dealloc() ARRAY_NOEXCEPT {
 		if(fElements != nullptr) {
 			delete[] fElements;
 			fElements = nullptr;
@@ -200,7 +230,7 @@ namespace lah {
 		}
 	}
 	template<class TYPE>
-	void Array6<TYPE>::dealloc() {
+	void Array6<TYPE>::dealloc() ARRAY_NOEXCEPT {
 		if(fElements != nullptr) {
 			delete[] fElements;
 			fElements = nullptr;
@@ -208,7 +238,7 @@ namespace lah {
 		}
 	}
 	template<class TYPE>
-	void Array7<TYPE>::dealloc() {
+	void Array7<TYPE>::dealloc() ARRAY_NOEXCEPT {
 		if(fElements != nullptr) {
 			delete[] fElements;
 			fElements = nullptr;
@@ -222,21 +252,22 @@ namespace lah {
 		if(n1<0 || n1>=fN1) ARRAY_ERROR("");
 #endif
 		return *(fElements + n1);
-	};
+	}
 	template<class TYPE>
 	TYPE &Array1<TYPE>::operator()(int n1) {
 #ifdef ARRAY_CHECK_BOUND
 		if(n1<0 || n1>=fN1) ARRAY_ERROR("");
 #endif
 		return *(fElements + n1);
-	};
+	}
+
 	template<class TYPE>
 	TYPE const &Array1<TYPE>::operator()(int n1) const {
 #ifdef ARRAY_CHECK_BOUND
 		if (n1<0 || n1 >= fN1) ARRAY_ERROR("");
 #endif
 		return *(fElements + n1);
-	};
+	}
 	template<class TYPE>
 	TYPE &Array2<TYPE>::at(int n1, int n2) {
 #ifdef ARRAY_CHECK_BOUND
@@ -330,7 +361,7 @@ namespace lah {
 		return *(fElements + (((n1*fN2 + n2)*fN3 + n3)*fN4 + n4)*fN5 + n5);
 	}
 	template<class TYPE>
-	TYPE &Array5<TYPE>::operator()(int n1, int n2, int n3, int n4, int n5) {
+	TYPE &Array5<TYPE>::operator()(int n1, int n2, int n3, int n4, int n5) ARRAY_NOEXCEPT{
 #ifdef ARRAY_CHECK_BOUND
 		if(n1<0 || n1>=fN1) ARRAY_ERROR("");
 		if(n2<0 || n2>=fN2) ARRAY_ERROR("");
@@ -341,7 +372,7 @@ namespace lah {
 		return *(fElements + (((n1*fN2 + n2)*fN3 + n3)*fN4 + n4)*fN5 + n5);
 	}
 	template<class TYPE>
-	TYPE const &Array5<TYPE>::operator()(int n1, int n2, int n3, int n4, int n5) const {
+	TYPE const &Array5<TYPE>::operator()(int n1, int n2, int n3, int n4, int n5) const ARRAY_NOEXCEPT {
 #ifdef ARRAY_CHECK_BOUND
 		if (n1<0 || n1 >= fN1) ARRAY_ERROR("");
 		if (n2<0 || n2 >= fN2) ARRAY_ERROR("");
@@ -364,7 +395,7 @@ namespace lah {
 		return *(fElements + ((((n1*fN2 + n2)*fN3 + n3)*fN4 + n4)*fN5 + n5)*fN6 + n6);
 	}
 	template<class TYPE>
-	TYPE &Array6<TYPE>::operator()(int n1, int n2, int n3, int n4, int n5, int n6) {
+	TYPE &Array6<TYPE>::operator()(int n1, int n2, int n3, int n4, int n5, int n6) ARRAY_NOEXCEPT {
 #ifdef ARRAY_CHECK_BOUND
 		if(n1<0 || n1>=fN1) ARRAY_ERROR("");
 		if(n2<0 || n2>=fN2) ARRAY_ERROR("");
@@ -376,7 +407,7 @@ namespace lah {
 		return *(fElements + ((((n1*fN2 + n2)*fN3 + n3)*fN4 + n4)*fN5 + n5)*fN6 + n6);
 	}
 	template<class TYPE>
-	TYPE const &Array6<TYPE>::operator()(int n1, int n2, int n3, int n4, int n5, int n6) const {
+	TYPE const &Array6<TYPE>::operator()(int n1, int n2, int n3, int n4, int n5, int n6) const ARRAY_NOEXCEPT {
 #ifdef ARRAY_CHECK_BOUND
 		if (n1<0 || n1 >= fN1) ARRAY_ERROR("");
 		if (n2<0 || n2 >= fN2) ARRAY_ERROR("");
@@ -401,7 +432,7 @@ namespace lah {
 		return *(fElements + (((((n1*fN2 + n2)*fN3 + n3)*fN4 + n4)*fN5 + n5)*fN6 + n6)*fN7 + n7);
 	}
 	template<class TYPE>
-	TYPE &Array7<TYPE>::operator()(int n1, int n2, int n3, int n4, int n5, int n6, int n7) {
+	TYPE &Array7<TYPE>::operator()(int n1, int n2, int n3, int n4, int n5, int n6, int n7) ARRAY_NOEXCEPT {
 #ifdef ARRAY_CHECK_BOUND
 		if(n1<0 || n1>=fN1) ARRAY_ERROR("");
 		if(n2<0 || n2>=fN2) ARRAY_ERROR("");
@@ -414,7 +445,7 @@ namespace lah {
 		return *(fElements + (((((n1*fN2 + n2)*fN3 + n3)*fN4 + n4)*fN5 + n5)*fN6 + n6)*fN7 + n7);
 	}
 	template<class TYPE>
-	TYPE const &Array7<TYPE>::operator()(int n1, int n2, int n3, int n4, int n5, int n6, int n7) const {
+	TYPE const &Array7<TYPE>::operator()(int n1, int n2, int n3, int n4, int n5, int n6, int n7) const ARRAY_NOEXCEPT {
 #ifdef ARRAY_CHECK_BOUND
 		if (n1<0 || n1 >= fN1) ARRAY_ERROR("");
 		if (n2<0 || n2 >= fN2) ARRAY_ERROR("");
@@ -426,99 +457,7 @@ namespace lah {
 #endif
 		return *(fElements + (((((n1*fN2 + n2)*fN3 + n3)*fN4 + n4)*fN5 + n5)*fN6 + n6)*fN7 + n7);
 	}
-	template<class TYPE>
-	void Array1<TYPE>::copy(TYPE const *data, int N) {
-		if(fN1 != N) {
-			dealloc();
-			alloc(N);
-		}
-		for(int i = 0; i < N; ++i) {
-			fElements[i] = data[i];
-		}
-	}
-	template<class TYPE>
-	void Array1<TYPE>::copy(Array1<TYPE> const &r) {
-		copy(r.fElements, r.fN1);
-	}
 
-
-	template<class TYPE>
-	void Array1<TYPE>::zeroize() {
-		::memset(fElements, 0, sizeof(TYPE)*fN1);
-	}
-	template<class TYPE>
-	void Array1<TYPE>::assign_zero() {
-		int N = fN1;
-		for(int i = 0; i < N; ++i) {
-			fElements[i] = 0;
-		}
-	}
-	template<class TYPE>
-	void Array2<TYPE>::zeroize() {
-		::memset(fElements, 0, sizeof(TYPE)*fN1*fN2);
-	}
-	template<class TYPE>
-	void Array2<TYPE>::assign_zero() {
-		int N = fN1*fN2;
-		for(int i = 0; i < N; ++i) {
-			fElements[i] = 0;
-		}
-	}
-	template<class TYPE>
-	void Array3<TYPE>::zeroize() {
-		::memset(fElements, 0, sizeof(TYPE)*fN1*fN2*fN3);
-	}
-	template<class TYPE>
-	void Array3<TYPE>::assign_zero() {
-		int N = fN1*fN2*fN3;
-		for(int i = 0; i < N; ++i) {
-			fElements[i] = 0;
-		}
-	}
-	template<class TYPE>
-	void Array4<TYPE>::zeroize() {
-		::memset(fElements, 0, sizeof(TYPE)*fN1*fN2*fN3*fN4);
-	}
-	template<class TYPE>
-	void Array4<TYPE>::assign_zero() {
-		int N = fN1*fN2*fN3*fN4;
-		for(int i = 0; i < N; ++i) {
-			fElements[i] = 0;
-		}
-	}
-	template<class TYPE>
-	void Array5<TYPE>::zeroize() {
-		::memset(fElements, 0, sizeof(TYPE)*fN1*fN2*fN3*fN4*fN5);
-	}
-	template<class TYPE>
-	void Array5<TYPE>::assign_zero() {
-		int N = fN1*fN2*fN3*fN4*fN5;
-		for(int i = 0; i < N; ++i) {
-			fElements[i] = 0;
-		}
-	}
-	template<class TYPE>
-	void Array6<TYPE>::zeroize() {
-		::memset(fElements, 0, sizeof(TYPE)*fN1*fN2*fN3*fN4*fN5*fN6);
-	}
-	template<class TYPE>
-	void Array6<TYPE>::assign_zero() {
-		int N = fN1*fN2*fN3*fN4*fN5*fN6;
-		for(int i = 0; i < N; ++i) {
-			fElements[i] = 0;
-		}
-	}
-	template<class TYPE>
-	void Array7<TYPE>::zeroize() {
-		::memset(fElements, 0, sizeof(TYPE)*fN1*fN2*fN3*fN4*fN5*fN6*fN7);
-	}
-	template<class TYPE>
-	void Array7<TYPE>::assign_zero() {
-		int N = fN1*fN2*fN3*fN4*fN5*fN6*fN7;
-		for(int i = 0; i < N; ++i) {
-			fElements[i] = 0;
-		}
-	}
 	template<class TYPE>
 	int Array1<TYPE>::size1() const {
 		return fN1;
